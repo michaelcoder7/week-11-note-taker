8cbb654c6dd7e4c63c080b23ea4352157bacb98f
Michael Coder7 <michaelokikiade@yahoo.co.uk>
2023-07-28 19:24:00 +0100

    notes.js code completed

--- routes/notes.js
+++ routes/notes.js
@@ -34,4 +34,23 @@ notes.delete("/:note_id", (req, res) => {
     });
 });
 
-notes;
+notes.post("/", (req, res) => {
+  console.log(req.body);
+
+  const { title, text } = req.body;
+
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+      id: uuidv4(),
+    };
+
+    readAndAppend(newNote, "./db/db.json");
+    res.json(`Note added successfully`);
+  } else {
+    res.errored("Error in adding Note");
+  }
+});
+
+module.exports = notes;
