07ea1f658b224fb29a032c9aec8c8b88969b5f49
Michael Coder7 <michaelokikiade@yahoo.co.uk>
2023-07-28 19:00:42 +0100

    notes.js in progress, new helpers folder added

---
+++ routes/notes.js
@@ -0,0 +1,12 @@
+const notes = require("express").Router();
+const { v4: uuidv4 } = require("uuid");
+const {
+  readFromFile,
+  readAndAppend,
+  writeToFile,
+} = require("../helpers/fsUtils");
+
+notes.get("/", (req, res) => {
+  console.info(`${req.method} request received for notes`);
+  readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
+});
